1c2a8333a3da42abd676ed99619e1b68be152970
Brian Christian <brchristian@gmail.com>
2017-03-04 11:02:53 -0800

    update adjustments_decorator to match current ItemAdjustments syntax

--- app/models/spree/item_adjustments_decorator.rb
+++ app/models/spree/item_adjustments_decorator.rb
@@ -1,20 +1,16 @@
 Spree::ItemAdjustments.class_eval do
-  define_callbacks :handling_adjustments
-
   module OverrideItemAdjustments
     def update_adjustments
       super
 
-      handling_total = 0
-      run_callbacks :handling_adjustments do
-        handling_total = adjustments.handling.reload.map(&:update!).compact.sum
-      end
+      @item.handling_total = adjustments.handling.reload.map(&:update!).compact.sum
+      @item.adjustment_total += @item.handling_total
 
-      item.update_columns(
-        handling_total: handling_total,
-        adjustment_total: item.adjustment_total + handling_total,
-        updated_at: Time.now
-      )
+      @item.update_columns(
+        handling_total: @item.handling_total,
+        adjustment_total: @item.adjustment_total,
+        updated_at: Time.current
+      ) if @item.changed?
     end
   end
 
