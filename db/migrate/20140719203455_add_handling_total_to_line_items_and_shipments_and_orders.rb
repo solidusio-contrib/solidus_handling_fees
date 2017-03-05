e47fb0692dead392640565bc867c6c5146212439
Brian Christian <brchristian@gmail.com>
2017-03-04 22:45:26 -0800

    gemspec for Solidus 1.0

--- db/migrate/20140719203455_add_handling_total_to_line_items_and_shipments_and_orders.rb
+++ db/migrate/20140719203455_add_handling_total_to_line_items_and_shipments_and_orders.rb
@@ -1,4 +1,4 @@
-class AddHandlingTotalToLineItemsAndShipmentsAndOrders < ActiveRecord::Migration[4.2]
+class AddHandlingTotalToLineItemsAndShipmentsAndOrders < ActiveRecord::Migration
   def change
     add_column :spree_line_items, :handling_total, :decimal, precision: 10, scale: 2, default: 0.0
     add_column :spree_shipments, :handling_total, :decimal, precision: 10, scale: 2, default: 0.0
