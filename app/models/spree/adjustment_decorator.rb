f0ae029d19f857bb4ed76ce97c8ec7e78ce51c88
Brian Christian <brchristian@gmail.com>
2017-03-07 13:24:14 -0800

    typo fix

--- app/models/spree/adjustment_decorator.rb
+++ app/models/spree/adjustment_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Adjustment.class_eval do
   scope :handling, -> { where(source_type: 'Spree::StockLocation') }
   
-  # @return [Boolean] true when this is a handlign adjustment (Handling adjustments have a {StockLocation} source)
+  # @return [Boolean] true when this is a handling adjustment (Handling adjustments have a {StockLocation} source)
   def handling?
     source_type == 'Spree::StockLocation'
   end
