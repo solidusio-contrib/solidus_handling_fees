ad2f5181b32830b4cc750e581346c218b4f68df9
Brian Christian <brchristian@gmail.com>
2018-03-07 17:04:34 -0800

    add deface original hash

--- app/overrides/add_handling_fee_to_order_summary.rb
+++ app/overrides/add_handling_fee_to_order_summary.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   name: 'add_handling_fee_to_order_summary',
   virtual_path: 'spree/checkout/_summary',
   insert_after: "[data-hook='order_details_shipment_promotion_adjustments']",
-  partial: 'spree/handling_fee_in_order_summary'
+  partial: 'spree/handling_fee_in_order_summary',
+  original: '2bbc6a5307205ad8013ae7d265c0587e17585722'
 )
