9635805ea5365acec084af73d605dcc7cc4e9f32
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-06-09 13:47:35 -0600

    Removed deprecated files

--- lib/spree_handling_fees.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-warn \
-  'DEPRECATED REQUIRE: Please update your refs from "spree_handling_fees" ' \
-  'to "solidus_handling_fees".', uplevel: 1
-require 'solidus_handling_fees'
